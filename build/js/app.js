77da0368d41c0906c4c4381ec49223d85b15f650
chris martinez and seth kendall <seth@>
2016-10-12 11:16:56 -0700

    displays results of search on page

--- build/js/app.js
+++ build/js/app.js
@@ -3862,17 +3862,20 @@ var app = new Clarifai.App(
 );
 
 
-var result;
+
 $(function(){
   $("#submit_button").click(function(){
     var url = $("#url").val();
     console.log(url);
+    $("#inputImage").attr("src", url);
     app.models.predict(Clarifai.GENERAL_MODEL, url).then(
       function(response) {
-        result = response;
         console.log(response);
-        var sample_output = response.data.outputs[0].data.concepts[0].name;
-        console.log(sample_output);
+        $("#tag1").text(response.data.outputs[0].data.concepts[0].name);
+        $("#tag2").text(response.data.outputs[0].data.concepts[1].name);
+        $("#tag3").text(response.data.outputs[0].data.concepts[2].name);
+        $("#tag4").text(response.data.outputs[0].data.concepts[3].name);
+        $("#tag5").text(response.data.outputs[0].data.concepts[4].name);
       },
       function(err) {
         console.error(err);
